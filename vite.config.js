8b0c1379737486e0b7635d9078a6fc303b0d9fe2
Filipe Guimarães dos Santos Teixeira <filipegst@live.com>
2025-03-31 20:56:08 -0300

    Testando vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import Inspect from 'vite-plugin-inspect'
+import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default {
-  plugins: [Inspect()],
+export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         cafes: resolve(__dirname, 'cafes.html'),
         conhecimentos: resolve(__dirname, 'conhecimentos.html')
-
+      },
+      output: {
+        assetFileNames: 'assets/[name].[ext]', // Garante que CSS e JS fiquem organizados
+        entryFileNames: 'assets/[name].js'
       }
     }
   }
-}
+})
